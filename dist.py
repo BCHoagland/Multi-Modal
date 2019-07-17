51ebf1e61e135ba6f5797be3b84686819ba87e59
Braden Hoagland <bhoggland@gmail.com>
2019-07-17 15:04:38 -0400

    better training now

--- dist.py
+++ dist.py
@@ -2,13 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
-from math import pi
-
-
-
-# need a way of making sure std's are positive
-
-
+from math import pi, isinf
 
 class Dist(nn.Module):
     def __init__(self, a=None, m=None, s=None, K=None, requires_grad=False):
@@ -21,17 +15,18 @@ class Dist(nn.Module):
         assert(all(len(x) == self.K for x in given))
 
         def init_weights(x, log=False):
-            w = torch.FloatTensor(x) if x is not None else torch.rand(self.K)
+            if x is not None:
+                w = torch.FloatTensor(x)
+                if log: w = torch.log(w)
+            else:
+                w = torch.rand(self.K)
             assert(w.shape == (self.K,))
-            if log: w = torch.log(w)
             w.requires_grad = requires_grad
             return w
 
         self.α = init_weights(a)
         self.μ = init_weights(m)
-        self.σ = init_weights(s, log=False)
-
-        self.dists = [Normal(m, s) for (m, s) in zip(self.μ, self.σ)]
+        self.σ = init_weights(s, log=True)
 
     def __str__(self):
         return f'Normal distribution with parameters\n\tα: {self.α.tolist()}\n\tμ: {self.μ.tolist()}\n\tσ: {self.σ.exp().tolist()}'
@@ -47,15 +42,16 @@ class Dist(nn.Module):
     def sample(self, batch_size=1):
         a = F.softmax(self.α, dim=0)
         i = torch.multinomial(a, 1)
-        dist = Normal(self.μ[i], self.σ[i])
+        dist = Normal(self.μ[i], self.σ[i].exp())
         return dist.sample((batch_size,))
 
     def log_prob(self, sample):
         dists = [Normal(m, s.exp()) for (m, s) in zip(self.μ, self.σ)]
         p = torch.stack([dist.log_prob(sample).exp() for dist in dists]).squeeze()
+        p = torch.clamp(p, 1e-10, 1)
         a = F.softmax(self.α, dim=0).unsqueeze(1)
         return torch.sum(p * a, dim=0).log()
 
     def N(self, x):
         if len(x.shape) == 1: x = torch.FloatTensor(x).unsqueeze(1)
-        return torch.exp(-torch.pow(x - self.μ, 2) / (2 * torch.pow(self.σ, 2))) / torch.sqrt(2 * pi * torch.pow(self.σ, 2))
+        return torch.exp(-torch.pow(x - self.μ, 2) / (2 * torch.pow(self.σ.exp(), 2))) / torch.sqrt(2 * pi * torch.pow(self.σ.exp(), 2))
--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 import torch
-import torch.nn.functional as F
 
 from dist import Dist
 from visualize import *
 
 epochs = 20000
-lr = 0.01
+lr = 0.05
 batch_size = 128
+vis_iter = 200
 
 # make target distribution
 P = Dist([.2, .2, .3, .1, .2], [-3, 3, 15, 10, -10], [1, 2, 3, 1, 5])
@@ -22,16 +22,12 @@ optimizer = torch.optim.Adam(Q.parameters(), lr=lr)
 # run optimization steps on batches of output from target distribution
 for epoch in range(int(epochs)):
     x = P.sample(batch_size)
-    loss = torch.pow(Q(x) - P(x), 2).mean()
-    # loss += torch.pow(P.log_prob(x) - Q.log_prob(x), 2).mean()
-    loss += -Q.log_prob(x).mean() * max(0, (1 - (epoch / (epochs / 2))))                        # anneal entropy bonus for smoother final fitting
-
-                                                                                                # detect when it's okay to switch over to just MSE
+    loss = torch.pow(Q(x) - P(x), 2).mean()                                             # what about cross entropy instead
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-    if epoch % (epochs // 200) == (epochs // 200) - 1:
+    if epoch % vis_iter == vis_iter - 1:
         plot_loss(loss, '#4A04D4')
         plot_dist(Q, 'Q', '#CB7EFF')
